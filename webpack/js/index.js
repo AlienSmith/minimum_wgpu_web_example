e8dc4b3fc130905ec3cd753cc731ac5ccc0f3882
朱安 <zhua01@vanke.com>
2024-11-22 15:22:59 +0800

    update

--- webpack/js/index.js
+++ webpack/js/index.js
@@ -1,5 +1,8 @@
 import init,{run} from "../../wasm/pkg/wgpu_wasm_test.js";
 Error.stackTraceLimit = 100;
-window.addEventListener("load", () => {
-  init();
+
+//this is a top level await which will pause execution of the js until the await function is done
+await init().then(() => {
+    console.log("WASM Loaded");
 });
+
